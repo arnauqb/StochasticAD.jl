18d645622fc6a466e5838f48aa474cda8f3c3655
Arnau Quera-Bofarull <arnauq@protonmail.com>
2024-06-14 09:49:19 +0100

    trying to implement 2d indices

--- src/general_rules.jl
+++ src/general_rules.jl
@@ -4,7 +4,7 @@ Operators which have already been overloaded by StochasticAD.
 const handled_ops = Tuple{DataType, Int}[]
 
 """
-    define_triple_overload(sig)
+	define_triple_overload(sig)
 
 Given the signature type-type of the primal function, define operator
 overloading rules for stochastic triples.
@@ -208,7 +208,7 @@ end
 
 for op in RNG_TYPEFUNCS_WRAP
     function (::typeof(op))(rng::AbstractRNG,
-        ::Type{StochasticTriple{T, V, FIs}}) where {T, V, FIs}
+            ::Type{StochasticTriple{T, V, FIs}}) where {T, V, FIs}
         return StochasticTriple{T, V, FIs}(op(rng, V), zero(V), empty(FIs))
     end
 end
@@ -225,15 +225,15 @@ way. (E.g. by catching the chain rule for isapprox and recursively calling isapp
 on the values.)
 =#
 function Base.isapprox(st1::StochasticTriple, st2::StochasticTriple;
-    atol::Real = 0, rtol::Real = Base.rtoldefault(st1, st2, atol),
-    nans::Bool = false, norm::Function = abs)
+        atol::Real = 0, rtol::Real = Base.rtoldefault(st1, st2, atol),
+        nans::Bool = false, norm::Function = abs)
     (isfinite(st1) && isfinite(st2) &&
      norm(st1 - st2) <= max(atol, rtol * max(norm(st1), norm(st2)))) ||
         (nans && isnan(st1) && isnan(st2))
 end
 function Base.isapprox(st1::StochasticTriple, x::Real;
-    atol::Real = 0, rtol::Real = Base.rtoldefault(st1, x, atol),
-    nans::Bool = false, norm::Function = abs)
+        atol::Real = 0, rtol::Real = Base.rtoldefault(st1, x, atol),
+        nans::Bool = false, norm::Function = abs)
     (isfinite(st1) && isfinite(x) &&
      norm(st1 - x) <= max(atol, rtol * max(norm(st1), norm(x)))) ||
         (nans && isnan(st1) && isnan(x))
@@ -246,7 +246,7 @@ end
 _isassigned(C, i) = (i in eachindex(C))
 
 """
-    Base.getindex(C::AbstractArray, st::StochasticTriple{T})
+	Base.getindex(C::AbstractArray, st::StochasticTriple{T})
 
 A simple prototype rule for array indexing. Assumes that underlying type of `st` can index into collection C.
 """
@@ -278,3 +278,181 @@ function Base.getindex(C::AbstractArray, st::StochasticTriple{T, V, FIs}) where 
     end
     return StochasticTriple{T}(value(val), delta(val), Δs)
 end
+
+function Base.getindex(
+        C::AbstractArray, st::StochasticTriple{T, V, FIs}, i2) where {T, V, FIs}
+    val = C[st.value, i2]
+    do_map = (Δ, state) -> begin
+        return value(C[st.value + Δ, i2], state) - value(val, state)
+    end
+    is_assigned(C, i) = 1 <= i <= size(C, 1)
+    # TODO: below doesn't support sparse arrays, use something like nextind
+    deriv = δ -> begin
+        scale = if is_assigned(C, st.value + 1) && is_assigned(C, st.value - 1)
+            1 / 2 * (value(C[st.value + 1, i2]) - value(C[st.value - 1, i2]))
+        elseif is_assigned(C, st.value + 1)
+            value(C[st.value + 1, i2]) - value(C[st.value, i2])
+        elseif is_assigned(C, st.value - 1)
+            value(C[st.value, i2]) - value(C[st.value - 1, i2])
+        else
+            zero(eltype(C))
+        end
+        return scale * δ
+    end
+
+    Δs = StochasticAD.map_Δs(do_map, st.Δs; deriv, out_rep = value(val))
+    if val isa StochasticTriple
+        Δs = combine((Δs, val.Δs))
+    end
+    return StochasticTriple{T}(value(val), delta(val), Δs)
+end
+
+function Base.getindex(
+        C::AbstractArray, i1, st::StochasticTriple{T, V, FIs}) where {T, V, FIs}
+    val = C[i1, st.value]
+    do_map = (Δ, state) -> begin
+        return value(C[i1, st.value + Δ], state) - value(val, state)
+    end
+    is_assigned(C, i) = 1 <= i <= size(C, 2)
+    # TODO: below doesn't support sparse arrays, use something like nextind
+    deriv = δ -> begin
+        scale = if is_assigned(C, st.value + 1) && is_assigned(C, st.value - 1)
+            1 / 2 * (value(C[i1, st.value + 1]) - value(C[i1, st.value - 1]))
+        elseif is_assigned(C, st.value + 1)
+            value(C[i1, st.value + 1]) - value(C[i1, st.value])
+        elseif is_assigned(C, st.value - 1)
+            value(C[i1, st.value]) - value(C[i1, st.value - 1])
+        else
+            zero(eltype(C))
+        end
+        return scale * δ
+    end
+
+    Δs = StochasticAD.map_Δs(do_map, st.Δs; deriv, out_rep = value(val))
+    if val isa StochasticTriple
+        Δs = combine((Δs, val.Δs))
+    end
+    return StochasticTriple{T}(value(val), delta(val), Δs)
+end
+
+function Base.getindex(C::AbstractArray, i1::StochasticTriple{T, V, FIs},
+        i2::StochasticTriple{T, V, FIs}) where {T, V, FIs}
+    val = C[i1.value, i2.value]
+    do_map_1 = (Δ, state) -> begin
+        return value(C[i1.value + Δ, i2.value], state) - value(val, state)
+    end
+    do_map_2 = (Δ, state) -> begin
+        return value(C[i1.value, i2.value + Δ], state) - value(val, state)
+    end
+    is_assigned_1(C, i) = 1 <= i <= size(C, 1)
+    is_assigned_2(C, i) = 1 <= i <= size(C, 2)
+    # TODO: below doesn't support sparse arrays, use something like nextind
+    deriv_1 = δ -> begin
+        scale = if is_assigned_1(C, i1.value + 1) && is_assigned_1(C, i1.value - 1)
+            1 / 2 * (value(C[i1.value + 1, i2.value]) - value(C[i1.value - 1, i2.value]))
+        elseif is_assigned_1(C, i1.value + 1)
+            value(C[i1.value + 1, i2.value]) - value(C[i1.value, i2.value])
+        elseif is_assigned_1(C, st.value - 1)
+            value(C[i1.value, i2.value]) - value(C[i1.value - 1, i2.value])
+        else
+            zero(eltype(C))
+        end
+        return scale * δ
+    end
+    deriv_2 = δ -> begin
+        scale = if is_assigned_2(C, i2.value + 1) && is_assigned_2(C, i2.value - 1)
+            1 / 2 * (value(C[i1.value, i2.value + 1]) - value(C[i1.value, i2.value - 1]))
+        elseif is_assigned_2(C, i2.value + 1)
+            value(C[i1.value, i2.value + 1]) - value(C[i1.value, i2.value])
+        elseif is_assigned_2(C, i2.value - 1)
+            value(C[i1.value, i2.value]) - value(C[i1.value, i2.value - 1])
+        else
+            zero(eltype(C))
+        end
+        return scale * δ
+    end
+
+    Δs_1 = StochasticAD.map_Δs(do_map_1, i1.Δs; deriv_1, out_rep = value(val))
+    Δs_2 = StochasticAD.map_Δs(do_map_2, i2.Δs; deriv_2, out_rep = value(val))
+    # randomly choose one of the two to be the "main" perturbation. Should be weighted by their relative weight
+    w1 = Δs_1.state.weight
+    w2 = Δs_2.state.weight
+    if rand() < w1 / (w1 + w2)
+        Δs = Δs_1
+    else
+        Δs = Δs_2
+    end
+    #Δs = combine((Δs_1, Δs_2))
+    if val isa StochasticTriple
+        Δs = combine((Δs, val.Δs))
+    end
+    return StochasticTriple{T}(value(val), delta(val), Δs)
+end
+
+function Base.getindex(
+        C::AbstractArray, i1::StochasticTriple{T, V, FIs}, i2::Colon) where {T, V, FIs}
+    [C[i1, i] for i in 1:size(C, 2)]
+end
+function Base.getindex(
+        C::AbstractArray, i1::Colon, i2::StochasticTriple{T, V, FIs}) where {T, V, FIs}
+    [C[i, i2] for i in 1:size(C, 1)]
+end
+function Base.getindex(C::AbstractArray, i1::Colon,
+        i2::Array{<:StochasticTriple{T, V, FIs}}) where {T, V, FIs}
+    return [C[i1, i] for i in i2]
+end
+function Base.getindex(C::AbstractArray, i1::Array{<:StochasticTriple{T, V, FIs}},
+        i2::Colon) where {T, V, FIs}
+    return [C[i, i2] for i in i1]
+end
+
+## old ##
+#function Base.getindex(C::AbstractArray, i1::StochasticTriple{T, V, FIs}, i2) where {T, V, FIs}
+#	i1_value = value(i1)
+#	val = C[i1_value, i2]
+#	function do_map(Δ, state)
+#		return value(C[i1_value+Δ, i2], state) - value(val, state)
+#	end
+#	Δs = map_Δs(do_map, i1.Δs)
+#	if val isa StochasticTriple
+#		Δs = combine((Δs, val.Δs))
+#	end
+#	return StochasticTriple{T}(value(val), delta(val), Δs)
+#end
+#function Base.getindex(C::AbstractArray, i1::StochasticTriple{T, V, FIs}, i2::StochasticTriple{T, V, FIs}) where {T, V, FIs}
+#	i1_value = value(i1)
+#    i2_value = value(i2)
+#	val = C[i1_value, i2_value]
+#	function do_map_i1(Δ, state)
+#		return value(C[i1_value+Δ, i2_value], state) - value(val, state)
+#	end
+#	function do_map_i2(Δ, state)
+#		return value(C[i1_value, i2_value + Δ], state) - value(val, state)
+#	end
+#	Δs = combine((map_Δs(do_map_i1, i1.Δs), map_Δs(do_map_i2, i2.Δs)))
+#	if val isa StochasticTriple
+#		Δs = combine((Δs, val.Δs))
+#	end
+#	return StochasticTriple{T}(value(val), delta(val), Δs)
+#end
+#Base.getindex(C::AbstractArray, i1::StochasticTriple{T, V, FIs}, i2::Colon) where {T, V, FIs} = [C[i1, i] for i in 1:size(C, 2)]
+#Base.getindex(C::AbstractArray, i1::Colon, i2::StochasticTriple{T, V, FIs}) where {T, V, FIs} = [C[i, i2] for i in 1:size(C, 1)]
+#function Base.getindex(C::AbstractArray, i1, i2::StochasticTriple{T, V, FIs}) where {T, V, FIs}
+#	i2_value = value(i2)
+#	val = C[i1, i2_value]
+#	function do_map(Δ, state)
+#		return value(C[i1, i2_value+Δ], state) - value(val, state)
+#	end
+#	Δs = StochasticAD.map_Δs(do_map, i2.Δs)
+#	if val isa StochasticTriple
+#		Δs = combine((Δs, val.Δs))
+#	end
+#	return StochasticTriple{T}(value(val), delta(val), Δs)
+#end
+#function Base.getindex(C::AbstractArray, i1::Colon, i2::Array{<:StochasticTriple{T, V, FIs}}) where {T, V, FIs}
+#	return [C[i1, i] for i in i2]
+#end
+#function Base.getindex(C::AbstractArray, i1::Array{<:StochasticTriple{T, V, FIs}}, i2::Colon) where {T, V, FIs}
+#	return [C[i, i2] for i in i1]
+#end
+#
